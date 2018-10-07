4da2fdbcffca75a1f9a59f41fcb8784e1da212ec
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 17:19:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  var obj = {};
+  obj = Object.assign({}, Object);
+  obj[key] = value;
+  return obj;
 }
